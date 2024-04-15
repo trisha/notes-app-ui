133748ff279545574e9ad226dbc5807d20b4bf18
trisha <29530389+trisha@users.noreply.github.com>
2024-04-14 22:03:02 -0700

    Add HTML and CSS to UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  return(
+    <div className="app-container">
+      <form className="note-form">
+        <input
+          placeholder="title"
+          required
+        ></input>
+        <textarea
+          placeholder="content"
+          rows={10}
+          required
+        ></textarea>
+        <button type="submit">
+          Add Note
+        </button>
+      </form>
+      <div className="notes-grid">
+        <div className="note-item">
+          <div className="notes-header">
+            <button>x</button>
+          </div>
+          <h2>Note Title</h2>
+          <p>Note Content</p>
+        </div>
+
+      </div>
     </div>
   );
-}
+};
 
 export default App;
