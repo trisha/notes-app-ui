fe9e4a85f8dafad3a3930aef5102392196fbec3b
trisha <29530389+trisha@users.noreply.github.com>
2024-04-15 11:33:17 -0700

    Add, edit, delete notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,175 @@
+import {useState} from 'react';
 import "./App.css"
 
+type Note = {
+  id: number;
+  title: string;
+  content: string;
+}
+
 const App = () => {
+  const [notes, setNotes] = useState<Note[]>([
+    {
+      id: 1,
+      title: "test note 1",
+      content: "bla bla note1",
+    },
+    {
+      id: 2,
+      title: "test note 2 ",
+      content: "bla bla note2",
+    },
+    {
+      id: 3,
+      title: "test note 3",
+      content: "bla bla note3",
+    },
+    {
+      id: 4,
+      title: "test note 4 ",
+      content: "bla bla note4",
+    },
+    {
+      id: 5,
+      title: "test note 5",
+      content: "bla bla note5",
+    },
+    {
+      id: 6,
+      title: "test note 6",
+      content: "bla bla note6",
+    },
+  ]);
+  
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const [selectedNote, setSelectedNote] = useState<Note | null>(null); // Can be empty
+
+  // Create new note
+  const handleSubmit = (
+    event: React.FormEvent
+  ) => {
+    event.preventDefault(); // prevent refresh
+    console.log("title", title);
+    console.log("content", content);
+
+    const newNote: Note = {
+      id: notes.length + 1,
+      title: title,
+      content: content
+    }
+
+    setNotes([newNote, ...notes]);
+    setTitle("");
+    setContent("");
+  };
+  
+  const handleNoteClick = (note:Note) => {
+    setSelectedNote(note);
+    setTitle(note.title);
+    setContent(note.content);
+  }
+
+  const handleUpdateNote = (
+    event: React.FormEvent
+  ) => {
+    event.preventDefault();
+
+    if(!selectedNote){
+      return;
+    }
+
+    const updatedNote: Note = {
+      id: selectedNote.id,
+      title: title,
+      content: content,
+    }
+
+    const updatedNotesList = notes.map((note) => // iterate thru array to find note we're looking for
+      note.id === selectedNote.id ? updatedNote : note)
+
+      setNotes(updatedNotesList);
+      setTitle("")
+      setContent("")
+      setSelectedNote(null);
+  }
+
+  const handleCancel = () => {
+    setTitle("")
+    setContent("")
+    setSelectedNote(null);
+  }    
+
+  const deleteNote = (
+    event: React.MouseEvent,
+    noteId: number
+  ) => {
+    event.stopPropagation(); // Want to prevent nested clicks in parent items, since delete button is inside a note
+
+    const updatedNotes = notes.filter(
+      (note) => note.id != noteId // returns all the notes that DON'T match the noteId
+    )
+
+    setNotes(updatedNotes);
+  }
+
   return(
     <div className="app-container">
-      <form className="note-form">
+      <form 
+        className="note-form"
+        onSubmit={(event) => 
+          selectedNote 
+            ? handleUpdateNote(event) 
+            : handleSubmit(event)
+          }
+      >
         <input
-          placeholder="title"
+          value={title}
+          onChange={(event) => 
+            setTitle(event.target.value)}
+          placeholder="Title"
           required
         ></input>
         <textarea
-          placeholder="content"
+          value={content}
+          onChange={(event) => 
+            setContent(event.target.value)}
+          placeholder="Content"
           rows={10}
           required
         ></textarea>
-        <button type="submit">
-          Add Note
-        </button>
+        {selectedNote ? (
+          <div className="edit-buttons">
+            <button type="submit">Save</button>
+            <button onClick={handleCancel}>
+              Cancel
+            </button>
+          </div>
+          ) : (
+          <button type="submit">
+            Add Note
+          </button>
+        )
+        }
       </form>
       <div className="notes-grid">
-        <div className="note-item">
+        {notes.map((note) => (
+          <div 
+            className="note-item"
+            onClick={() => handleNoteClick(note)}
+          >
           <div className="notes-header">
-            <button>x</button>
+            <button
+              onClick={(event) =>
+                deleteNote(event, note.id)
+              }
+            >x</button>
           </div>
-          <h2>Note Title</h2>
-          <p>Note Content</p>
+          <h2>{note.title}</h2>
+          <p>{note.content}</p>
         </div>
-
+        ))}
       </div>
     </div>
   );
